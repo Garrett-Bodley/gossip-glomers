eb1d30826381efe834343390247aca1d0b1dd01e
Garrett Bodley <garrett.bodley@gmail.com>
2024-05-29 17:00:14 -0400

    WIP: 3c is broken. Unclear why.

--- cmd/maelstrom-broadcast/main.go
+++ cmd/maelstrom-broadcast/main.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"sync"
 	"strings"
+	"fmt"
+	"context"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -25,8 +28,11 @@ func main() {
 	store := map[int]bool{}
 	mu := sync.Mutex{}
 	topology := make(map[string][]string)
-	n.Handle("broadcast", func(msg maelstrom.Message) error {
 
+	// TODO:
+	// [ ] Handler for broadcast_ok, read_ok ?
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var bodley struct {
 			Type    string
 			Message int
@@ -38,22 +44,43 @@ func main() {
 		store[bodley.Message] = true
 		mu.Unlock()
 
-		// TODO:
-		// [x] Fix concurrency issue
-		// [x] Blow up github repo
-		// [ ] Reduce unnecessary network chatter (gossip)
-
-
 		whoami := msg.Dest
 		from := msg.Src
 		for _, node := range n.NodeIDs() {
 			if whoami == node || from == node || strings.HasPrefix(from, "n") { continue }
+			if strings.HasPrefix(node, "c") { log.Printf("wtf why is this not logging ever the problem is not here: %s\n", node); panic(1) }
 
-			n.Send(node, msg.Body)
+			delay := time.Millisecond * 200
+			for i := 0; i < 10; i++ {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 2)
+				defer cancel()
+
+				msg2, err := n.SyncRPC(ctx, node, msg.Body)
+				if err != nil {
+					log.Printf("Maybe here? %+v\n", err)
+					time.Sleep(delay)
+					delay *= 2
+					log.Printf("Trying again %d\n", i)
+					continue
+				}
+
+				// overkill but it's fine
+				var kaplan struct {
+					Type string
+					Message int
+					MsgId int
+				}
+				if err := json.Unmarshal(msg2.Body, &kaplan); err != nil {
+					return err
+				}
+				if kaplan.Type != "broadcast_ok" {
+					panic(fmt.Sprintf("broadcast is not okay: %s", kaplan.Type))
+				}
+				break
+			}
+
 		}
-		// for neighbor := range topology[whoami] {
-		// 	n.Send(neighbor, msg)
-		// }
+
 
 		resp := map[string]any{}
 		resp["type"] = "broadcast_ok"
