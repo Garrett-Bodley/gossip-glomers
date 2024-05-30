bdbc7c507c58e269d76c862f2c6bdea0c4b512e0
Garrett Bodley <garrett.bodley@gmail.com>
2024-05-30 15:42:08 -0400

    IT WORKS.

--- cmd/maelstrom-broadcast/main.go
+++ cmd/maelstrom-broadcast/main.go
@@ -1,135 +1,100 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"sync"
-	"strings"
-	"fmt"
 	"context"
+	"encoding/json"
+	// "fmt"
+	"log"
+	"strings"
+	"sync"
 	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-// Your node will receive a request message body that looks like this:
-// {
-//   "type": "broadcast",
-//   "message": 1000
-// }
-
-// It should store the "message" value locally so it can be read later. In response, it should send an acknowledge with a broadcast_ok message:
-// {
-//   "type": "broadcast_ok"
-// }
-
-func main() {
+func main(){
 	n := maelstrom.NewNode()
-	store := map[int]bool{}
-	mu := sync.Mutex{}
-	topology := make(map[string][]string)
+	s := &server{n : n}
 
-	// TODO:
-	// [ ] Handler for broadcast_ok, read_ok ?
-
-	n.Handle("broadcast", func(msg maelstrom.Message) error {
-		var bodley struct {
-			Type    string
-			Message int
-		}
-		if err := json.Unmarshal(msg.Body, &bodley); err != nil {
-			return err
-		}
-		mu.Lock()
-		store[bodley.Message] = true
-		mu.Unlock()
-
-		whoami := msg.Dest
-		from := msg.Src
-		for _, node := range n.NodeIDs() {
-			if whoami == node || from == node || strings.HasPrefix(from, "n") { continue }
-			if strings.HasPrefix(node, "c") { log.Printf("wtf why is this not logging ever the problem is not here: %s\n", node); panic(1) }
-
-			delay := time.Millisecond * 200
-			for i := 0; i < 10; i++ {
-				ctx, cancel := context.WithTimeout(context.Background(), time.Second * 2)
-				defer cancel()
-
-				msg2, err := n.SyncRPC(ctx, node, msg.Body)
-				if err != nil {
-					log.Printf("Maybe here? %+v\n", err)
-					time.Sleep(delay)
-					delay *= 2
-					log.Printf("Trying again %d\n", i)
-					continue
-				}
-
-				// overkill but it's fine
-				var kaplan struct {
-					Type string
-					Message int
-					MsgId int
-				}
-				if err := json.Unmarshal(msg2.Body, &kaplan); err != nil {
-					return err
-				}
-				if kaplan.Type != "broadcast_ok" {
-					panic(fmt.Sprintf("broadcast is not okay: %s", kaplan.Type))
-				}
-				break
-			}
-
-		}
-
-
-		resp := map[string]any{}
-		resp["type"] = "broadcast_ok"
-
-		return n.Reply(msg, resp)
-	})
-
-	n.Handle("read", func(msg maelstrom.Message) error {
-		var nakamata struct {
-			Type string
-		}
-		if err := json.Unmarshal(msg.Body, &nakamata); err != nil {
-			return err
-		}
-
-		messages := []int{}
-
-		for key := range store {
-			messages = append(messages, key)
-		}
-
-		resp := map[string]any{}
-		resp["type"] = "read_ok"
-		resp["messages"] = messages
-
-		return n.Reply(msg, resp)
-	})
-
-	n.Handle("topology", func(msg maelstrom.Message) error {
-
-		var peter struct {
-			Type string
-			Topology map[string][]string
-		}
-		if err := json.Unmarshal(msg.Body, &peter); err != nil {
-			return err
-		}
-
-		for key, value := range peter.Topology {
-			topology[key] = value
-		}
-
-		resp := map[string]any{}
-		resp["type"] = "topology_ok"
-
-		return n.Reply(msg, resp)
-	})
+	n.Handle("broadcast", s.broadcastHandler)
+	n.Handle("read", s.readHandler)
+	n.Handle("topology", s.topologyHandler)
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
+
+type server struct {
+	n *maelstrom.Node
+
+	idsMu sync.RWMutex
+	ids   []int
+	topologyMu      sync.RWMutex
+	currentTopology map[string][]string
+}
+
+func (s *server) broadcastHandler(msg maelstrom.Message) error {
+	var body map[string]any
+	delay := time.Millisecond * 200
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	s.idsMu.Lock()
+	s.ids = append(s.ids, int(body["message"].(float64)))
+	s.idsMu.Unlock()
+	log.Printf("GLOMMING HERE (ﾉಥ益ಥ）ﾉ ┻━┻: %+v\n\n\n\n\n", s.n.NodeIDs())
+	for _, node := range s.n.NodeIDs() {
+		if node == msg.Dest || node == msg.Src || strings.HasPrefix(msg.Src, "n") { continue }
+		for i := 0; i < 10; i++{
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+
+			_, err := s.n.SyncRPC(ctx, node, msg.Body)
+			if err != nil {
+				time.Sleep(delay)
+				continue
+			}
+			break
+		}
+	}
+	// if strings.HasPrefix(msg.Src, "c") { return nil }
+	return s.n.Reply(msg, map[string]any{
+		"type": "broadcast_ok",
+	})
+}
+
+func (s *server) readHandler(msg maelstrom.Message) error {
+	s.idsMu.RLock()
+	ids := make([]int, len(s.ids))
+	for i := 0; i < len(s.ids); i++ {
+		ids[i] = s.ids[i]
+	}
+	s.idsMu.RUnlock()
+
+	return s.n.Reply(msg, map[string]any{
+		"type": "read_ok",
+		"messages": ids,
+	})
+}
+
+type topologyMsg struct {
+	Topology map[string][]string `json:"topology"`
+}
+
+func (s *server) topologyHandler(msg maelstrom.Message) error {
+	var t topologyMsg
+	if err := json.Unmarshal(msg.Body, &t); err != nil {
+		return err
+	}
+
+	s.topologyMu.Lock()
+	s.currentTopology = t.Topology
+	s.topologyMu.Unlock()
+
+	return s.n.Reply(msg, map[string]any{
+		"type": "topology_ok",
+	})
+}
+
